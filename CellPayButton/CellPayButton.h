6c574bf95cec158507a006853c774cc7f5eab179
Rahul Karanjit <rahul.karanjit@cellcom.net.np>
2020-07-30 13:27:13 +0545

    Initial Commit

---
+++ CellPayButton/CellPayButton.h
@@ -0,0 +1,19 @@
+//
+//  CellPayButton.h
+//  CellPayButton
+//
+//  Created by Cellcom on 7/30/20.
+//  Copyright © 2020 Cellcom. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CellPayButton.
+FOUNDATION_EXPORT double CellPayButtonVersionNumber;
+
+//! Project version string for CellPayButton.
+FOUNDATION_EXPORT const unsigned char CellPayButtonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CellPayButton/PublicHeader.h>
+
+
